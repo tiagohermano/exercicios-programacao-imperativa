981b028e679efb1920a8fa683b205e7c075faa02
Tiago Hermano <tiagofhermano@gmail.com>
2018-08-22 14:01:24 -0300

    ex 2 resolvido

--- Lista 1/ex2.c
+++ Lista 1/ex2.c
@@ -3,8 +3,8 @@ void main()
 {
     int controle;
     scanf("%d", &controle);
-    for(int index = 0;index < controle;index+=2)
+    for(int index = 1;index <= controle;index++)
     {
-        printf("%d", index);
+        printf("%d \n", 2*index);
     };
 }
