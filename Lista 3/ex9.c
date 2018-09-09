d86c887c703d45dbd1e7637ed059fffe1ad51610
Tiago Hermano <tiagofhermano@gmail.com>
2018-09-09 16:45:06 -0300

    Resolvido ex9

---
+++ Lista 3/ex9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+char * removerCaractere(char stringRemocao[], char caractereRemocao);
+
+void main() {
+  char stringRemocao[256], caractereRemocao;
+
+  printf("Digite uma string: ");
+  scanf("%[^\n]s", &stringRemocao);
+
+  printf("Forneca o caractere a ser removido: ");
+  scanf(" %c", &caractereRemocao);
+
+  printf("Nova String: %s", removerCaractere(stringRemocao, caractereRemocao));
+};
+
+char * removerCaractere(char stringRemocao[], char caractereRemocao) {
+  for(int i=0; i<strlen(stringRemocao); i++) {
+    if(stringRemocao[i] == caractereRemocao) {
+      stringRemocao[i] = '*';
+    };
+  };
+  return stringRemocao;
+};
